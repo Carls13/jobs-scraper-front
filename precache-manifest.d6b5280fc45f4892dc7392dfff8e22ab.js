9394c98e9dd3371431ad36277ded613eb1b3ef70
Carls13 <charlesshb98@gmail.com>
2020-06-18 23:40:56 -0400

    Updates

---
+++ precache-manifest.d6b5280fc45f4892dc7392dfff8e22ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acee4d44be249f304fa599bec99b895",
+    "url": "/jobs-scraper-front/index.html"
+  },
+  {
+    "revision": "1471988a1c4b63944e94",
+    "url": "/jobs-scraper-front/static/js/2.829ab166.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jobs-scraper-front/static/js/2.829ab166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f44dc9a45b37f5c35f",
+    "url": "/jobs-scraper-front/static/js/main.63704d15.chunk.js"
+  },
+  {
+    "revision": "28e4db9637310ea8b4dc",
+    "url": "/jobs-scraper-front/static/js/runtime-main.dc7ca196.js"
+  },
+  {
+    "revision": "bdd40cf9386a83e11660fab1e8fe9965",
+    "url": "/jobs-scraper-front/static/media/icon-hamburger.bdd40cf9.svg"
+  },
+  {
+    "revision": "77f4a9b334aa24a6a5f34bfd60be9d09",
+    "url": "/jobs-scraper-front/static/media/logo.77f4a9b3.png"
+  },
+  {
+    "revision": "63b2380fce2a05561da9cfb8b8095fdb",
+    "url": "/jobs-scraper-front/static/media/myself.63b2380f.JPG"
+  },
+  {
+    "revision": "4fedf7e16b99529b5c6bf84b447afae7",
+    "url": "/jobs-scraper-front/static/media/rocket.4fedf7e1.svg"
+  }
+]);
